7e47ffd0932009ac4e3aa5b2e64f2506f9629793
Doruk Demirci <dorukdemirci.boun@gmail.com>
2017-12-01 00:41:27 +0300

    Last changes

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,19 @@
 
-public class BankAccount {
+public abstract class BankAccount {
 
+	private int accountID;
+	protected double currentBalance ;
+	
+	public static void depositMoney(int accountID, double amount)
+	{
+		
+	}
+	
+	
+	public static void withdrawMoney(int accountID, double amount)
+	{
+		
+	}
+	
+	
 }
