42e0696f399c498c0b3ed55dd154b9e7fa43a29a
Doruk Demirci <dorukdemirci.boun@gmail.com>
2017-12-01 00:40:07 +0300

    Initial files have been added

---
+++ src/Bank.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Bank {
+	
+	 ArrayList<BankAccount> interestAccounts = new ArrayList<BankAccount>();
+	 ArrayList<BankAccount> nonInterestAccounts = new ArrayList<BankAccount>();
+	 
+	 // type = 0 for interest
+	 // else for nonInterest
+	 public static void openAccount(int type)
+	 {
+		 if(type == 0)
+		 {
+			 // interest account
+		 }
+		 else
+		 {
+			 // nonInterestAccount
+		 }
+	 }
+	 
+	 public static void closeAccount()
+	 {
+		 
+	 }
+	 
+	 // retrieve BankAccount
+	 public static BankAccount getBankAccount(int accountID)
+	 {
+		 BankAccount a = new BankAccount();
+		 
+		 return a;
+	 }
+
+}
