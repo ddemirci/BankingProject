fc9465883adbbafc32e1b1831184d1d33a512e24
Doruk Demirci <dorukdemirci.boun@gmail.com>
2017-12-19 01:14:18 +0300

    Menu interface was started to implement.

--- src/BankingProject.java
+++ src/BankingProject.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 
 public class BankingProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			
+			/*
 			Bank.setInterestRate(3.7);
 			Bank.openAccount(0);
 			System.out.println("Yaptık bisey");
@@ -20,6 +21,48 @@ public class BankingProject {
 			BankAccount.depositMoney(1, 2000.0);
 			System.out.println(Bank.nonInterestAccounts.get(0).currentBalance);
 			Bank.getAllBankAccountsInfo();
+			*/
+		
+		
+		
+		
+		// There will be a menu
+		int decision;
+		while(true)
+		{
+			Scanner scan = new Scanner(System.in);
+			showCustomerMenu();
+			decision = scan.nextInt();
+			
+			
+		}
+			 
+			  
+	}
+	public static void logIn(int customerID, String password)
+	{
+		// check the info from Excel
+	}
+	
+	public static void logOut()
+	{
+		
+	}
+	public static void showCustomerMenu()
+	{
+		System.out.println("Which operation do you want to proceed ? \n Please specify the number");
+		System.out.println("1. Open an account. \n2. Close an account.");
+		
+		System.out.println("");
+	}
+	
+	public static void showAdminMenu()
+	{
+		System.out.println("Which operation do you want to proceed ? \n Please specify the number");
+		System.out.println("1. Create a user. \n2.Delete a user.");
+		System.out.println("3. Set interest rate. \n4. Get interest rate.");
+		System.out.println("5. Retrieve an account in the bank. \n6. Retrieve all accounts in the bank.");
+		
 	}
 
 }
